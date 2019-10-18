8a1b08a6dea78682a58d4906135f51a2eb03dfc7
Steven Tarazona <ys.tarazona@uniandes.edu.co>
2019-10-18 00:24:04 -0500

    main

--- Caso2/src/main/java/ys/tarazona/Caso2/Cliente.java
+++ Caso2/src/main/java/ys/tarazona/Caso2/Cliente.java
@@ -1,45 +1,136 @@
 package ys.tarazona.Caso2;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.xml.bind.DatatypeConverter;
 
 public class Cliente {
 
-	public static final int PUERTO = 3400;
-	public static final String SERVIDOR = "localhost";
-	
-	public static void main(String[] args) throws IOException{
-		Socket socket = null;
-		PrintWriter escritor = null;
-		BufferedReader lector = null;
-		
-		System.out.println("Cliente ...");
-		
-		try {
-			socket = new Socket(SERVIDOR, PUERTO);
-			escritor = new PrintWriter(socket.getOutputStream(),true);
-			lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(-1);
+	private Socket socket;
+	private PrintWriter escritor;
+	private BufferedReader lector;
+	private BufferedReader stdIn;
+	private String[] algoritmos;
+	X509Certificate certificadoServidor;
+
+	public Cliente(Socket socket, PrintWriter escritor, BufferedReader lector, BufferedReader stdIn) {
+		this.socket = socket;
+		this.escritor = escritor;
+		this.lector = lector;
+		this.stdIn = stdIn;
+	}
+
+	public void procesar() throws IOException, CertificateException, InvalidKeyException, NoSuchAlgorithmException,
+			NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+
+		etapa1();
+		etapa2();
+		etapa3();
+		etapa4();
+	}
+
+	private void etapa1() throws IOException {
+		System.out.println("Escriba el mensaje para enviar:");
+		escritor.println(stdIn.readLine());
+		String fromServer = "";
+		if ((fromServer = lector.readLine()) != null) {
+			System.out.println("Respuesta del servidor: " + fromServer);
+			if (fromServer.equals("OK")) {
+				System.out.println("Seleccione algoritmos: (ALGORITMOS:<ALGS>:<ALGA>:<ALGHMAC>)");
+				System.out.println(
+						"-----------------------------------\nALGS:		DES, AES, Blowfish, RC4\nALGA:		RSA\nALGHMAC:	HMACSHA1, HMACSHA256, HMACSHA384, HMACSHA512\n-----------------------------------");
+				String line;
+				escritor.println(line=stdIn.readLine());
+				algoritmos = line.split(":");
+			}
 		}
-		
-		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-		
-		try {
-			ProtocoloCliente.procesar(stdIn, lector, escritor);
-		} catch (CertificateException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	}
+
+	private void etapa2() throws IOException, CertificateException, InvalidKeyException, NoSuchAlgorithmException,
+			NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+		String fromServer = "";
+		if ((fromServer = lector.readLine()) != null) {
+			System.out.println("Respuesta del servidor: " + fromServer);
+			if (fromServer.equals("OK") && (fromServer = lector.readLine()) != null) {
+				System.out.println("Certificado del Servidor: "
+						+ (certificadoServidor = generarCertificado(fromServer)).toString() + "\n");
+				certificadoServidor.checkValidity();
+				
+
+				escritor.println(cifrar(generarLlaveSimetrica().getEncoded(), certificadoServidor.getPublicKey(),algoritmos[2], 1));
+
+				System.out.println("Ingrese el reto:");
+				escritor.println(stdIn.readLine());
+				if ((fromServer = lector.readLine()) != null)
+					System.out.println("Respuesta del servidor: " + fromServer);
+				if ((fromServer = lector.readLine()) != null)
+					System.out.println("Respuesta del servidor: " + fromServer);
+				else
+					System.out.println("fuck");
+			}
 		}
-		
-		stdIn.close();
-		escritor.close();
-		lector.close();
-		socket.close();
+	}
+
+	private void etapa3() {
+
+	}
+
+	private void etapa4() {
+
+	}
+
+	/**
+	 * @param msg
+	 * @param key
+	 * @param algoritmo : DES, AES, Blowfish, RSA, ECIES, RC4, HMACMD5, HMACSHA1,
+	 *                  HMACSHA256, HMACSHA384, HMACSHA512
+	 * @param accion    : 1 (encriptar), 2(decriptar)
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchPaddingException
+	 * @throws InvalidKeyException
+	 * @throws IllegalBlockSizeException
+	 * @throws BadPaddingException
+	 */
+	private byte[] cifrar(byte[] msg, Key key, String algoritmo, int accion) throws NoSuchAlgorithmException,
+			NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		final Cipher cifrador = Cipher.getInstance(algoritmo);
+		cifrador.init(accion, key);
+		return cifrador.doFinal(msg);
+	}
+
+	private X509Certificate generarCertificado(String sertificado) throws CertificateException {
+		byte[] bytes = DatatypeConverter.parseBase64Binary(sertificado);
+		return (X509Certificate) CertificateFactory.getInstance("X.509")
+				.generateCertificate(new ByteArrayInputStream(bytes));
+	}
+
+	private SecretKey generarLlaveSimetrica() throws NoSuchAlgorithmException {
+		return KeyGenerator.getInstance(algoritmos[1]).generateKey();
+	}
+
+	private KeyPair generarLlavesAsimetricas() throws NoSuchAlgorithmException {
+		KeyPairGenerator llaves = KeyPairGenerator.getInstance(algoritmos[2]);
+		llaves.initialize(1024, new SecureRandom());
+		return llaves.generateKeyPair();
 	}
 }
---
+++ Caso2/src/main/java/ys/tarazona/Caso2/Principal.java
@@ -0,0 +1,101 @@
+package ys.tarazona.Caso2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public class Principal {
+
+	public static int PUERTO;
+	public static final String SERVIDOR = "localhost";
+	public static final String JAR = "servidor20192.jar";
+	public static Process servidor;
+	public static Socket socket;
+	public static PrintWriter escritor;
+	public static BufferedReader lector;
+	public static BufferedReader stdIn;
+
+	private static void inicializarServidor() throws IOException {
+		ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c",
+				"java -jar \"" + System.getProperty("user.dir") + "\\" + JAR);
+		builder.redirectErrorStream(true);
+		servidor = builder.start();
+
+		BufferedReader r = new BufferedReader(new InputStreamReader(servidor.getInputStream()));
+		PrintWriter w = new PrintWriter(servidor.getOutputStream(), true);
+		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		if ((line = r.readLine()) != null) {
+			System.out.println(line);
+			w.println(PUERTO = Integer.parseInt(stdIn.readLine()));
+			System.out.println(line = r.readLine());
+		}
+	}
+
+	private static void inicializarServidorCMD() throws IOException {
+		Runtime.getRuntime().exec(
+				"cmd.exe /c start cmd.exe /k \"java -jar \"" + System.getProperty("user.dir") + "\\" + JAR + "\"");
+		System.out.println("Eztablezca puerto de conexion (primero en el CMD):");
+		PUERTO = Integer.parseInt(stdIn.readLine());
+	}
+
+	private static void inicializar() throws UnknownHostException, IOException {
+		socket = new Socket(SERVIDOR, PUERTO);
+		escritor = new PrintWriter(socket.getOutputStream(), true);
+		lector = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	}
+
+	private static void finalizar() throws IOException {
+		stdIn.close();
+		escritor.close();
+		lector.close();
+		socket.close();
+	}
+
+	public static void main(String[] args) {
+
+		try {
+
+			stdIn = new BufferedReader(new InputStreamReader(System.in));
+			boolean consola = false;
+			while (!consola) {
+				System.out.println("Desea ver la consola del servidor? SI/NO");
+				String line = stdIn.readLine();
+				if (line.equals("SI")) {
+					inicializarServidorCMD();
+					consola = true;
+				} else if (line.equals("NO")) {
+					inicializarServidor();
+					consola = true;
+				} else
+					System.out.println("Entrada no valida");
+			}
+
+			inicializar();
+
+			Cliente cliente = new Cliente(socket, escritor, lector, stdIn);
+
+			try {
+				cliente.procesar();
+			} catch (CertificateException | InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException
+					| IllegalBlockSizeException | BadPaddingException e) {
+				e.printStackTrace();
+			}
+
+			finalizar();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		servidor.destroy();
+	}
+}
